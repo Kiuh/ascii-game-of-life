e23f12d3134ff7501f1eddf3c97efabb52141f95
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-08-21 22:53:19 +0300

    Add cross-compilation

--- build.zig
+++ build.zig
@@ -1,25 +1,32 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
 
-    const exe = b.addExecutable(.{
-        .name = "ascii-game-of-life",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+pub fn build(b: *std.Build) !void {
+    for (targets) |t| {
+        const exe = b.addExecutable(.{
+            .name = "ascii-game-of-life",
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(t),
+            .optimize = .ReleaseSafe,
+        });
 
-    b.installArtifact(exe);
+        b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
+        const target_output = b.addInstallArtifact(exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try t.zigTriple(b.allocator),
+                },
+            },
+        });
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        b.getInstallStep().dependOn(&target_output.step);
     }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }
