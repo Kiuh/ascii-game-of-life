08ce48a1b9083aef789b4028673edc0f3830580f
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-08-25 21:25:59 +0300

    Simplify build script and target zig version `0.13.0`

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A simple console implementation of [conway`s game of life](https://conwaylife.com/wiki/Conway%27s_Game_of_Life). Implemented in [zig lang](https://ziglang.org/), using [clap](https://github.com/Hejsil/zig-clap) for command line parsing.
 
+Tested with zig version 0.13.0
+
 # Test a game
 
 For downloading you can clone this repo.
@@ -9,11 +11,15 @@ For downloading you can clone this repo.
 ```
 git clone https://github.com/Kiuh/ascii-game-of-life
 ```
+
 Move to root project directory.
+
 ```
 cd ascii-game-of-life
 ```
+
 Then build and run game.
+
 ```
 zig build run
 ```
--- build.zig
+++ build.zig
@@ -1,56 +1,36 @@
 const std = @import("std");
 
-const targets: []const std.Target.Query = &.{
-    .{ .cpu_arch = .aarch64, .os_tag = .macos },
-    .{ .cpu_arch = .aarch64, .os_tag = .linux },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
-    .{ .cpu_arch = .x86_64, .os_tag = .windows },
-};
-
 pub fn build(b: *std.Build) !void {
     const root_source_file = b.path("src/main.zig");
-    const run_step = b.step("run", "Run the application");
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "ascii-game-of-life",
+        .root_source_file = root_source_file,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+
+    // Clap dependency
+    const clap = b.dependency("clap", .{});
+    exe.root_module.addImport("clap", clap.module("clap"));
+
+    // Add test command for current os type
+    const unit_tests = b.addTest(.{
+        .root_source_file = root_source_file,
+        .target = target,
+    });
+
+    // Add test command
     const test_step = b.step("test", "Run unit tests");
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 
-    for (targets) |t| {
-        const exe = b.addExecutable(.{
-            .name = "ascii-game-of-life",
-            .root_source_file = root_source_file,
-            .target = b.resolveTargetQuery(t),
-            .optimize = .ReleaseFast,
-        });
-
-        b.installArtifact(exe);
-
-        const clap = b.dependency("clap", .{});
-        exe.root_module.addImport("clap", clap.module("clap"));
-
-        const target_output = b.addInstallArtifact(exe, .{
-            .dest_dir = .{
-                .override = .{
-                    .custom = try t.zigTriple(b.allocator),
-                },
-            },
-        });
-
-        b.getInstallStep().dependOn(&target_output.step);
-
-        // Add test command for current os type
-        if (t.os_tag == .windows) {
-            const unit_tests = b.addTest(.{
-                .root_source_file = root_source_file,
-                .target = b.resolveTargetQuery(t),
-            });
-
-            const run_unit_tests = b.addRunArtifact(unit_tests);
-            test_step.dependOn(&run_unit_tests.step);
-        }
-
-        // Add run command for current os type
-        if (t.os_tag == .windows) {
-            const run_exe = b.addRunArtifact(exe);
-            run_step.dependOn(&run_exe.step);
-        }
-    }
+    // Add run command
+    const run_step = b.step("run", "Run the application");
+    const run_exe = b.addRunArtifact(exe);
+    run_step.dependOn(&run_exe.step);
 }
